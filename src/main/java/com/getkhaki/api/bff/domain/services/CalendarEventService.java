d95ddea8819940009a7e8082068bb434cac7ff01
harris Zinsouvi <harriszinsouvi@gmail.com>
2020-11-19 20:58:12 +0100

    new Change (calendar import tests) (#10)
    * new Change (calendar import tests)
    
    * new change
    
    Co-authored-by: Dinga Dinga <dingastream1120@gmail.com>

--- src/main/java/com/getkhaki/api/bff/domain/services/CalendarEventService.java
+++ src/main/java/com/getkhaki/api/bff/domain/services/CalendarEventService.java
@@ -2,19 +2,32 @@ package com.getkhaki.api.bff.domain.services;
 
 import com.getkhaki.api.bff.domain.models.CalendarEventDm;
 import com.getkhaki.api.bff.domain.persistence.CalendarEventPersistenceInterface;
+import com.getkhaki.api.bff.domain.persistence.CalendarProviderPersistenceFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CalendarEventService {
     private final CalendarEventPersistenceInterface calendarEventPersistence;
+    private final CalendarProviderPersistenceFactory calendarProviderPersistenceFactory;
 
     @Autowired
-    public CalendarEventService(CalendarEventPersistenceInterface calendarEventPersistence) {
+    public CalendarEventService(CalendarEventPersistenceInterface calendarEventPersistence, CalendarProviderPersistenceFactory calendarProviderPersistenceFactory) {
         this.calendarEventPersistence = calendarEventPersistence;
+        this.calendarProviderPersistenceFactory = calendarProviderPersistenceFactory;
     }
 
     public CalendarEventDm createEvent(CalendarEventDm calendarEventDm) {
         return calendarEventPersistence.createEvent(calendarEventDm);
     }
+
+
+    @Async("asyncExecutor")
+    public void importAsync(String adminEmail) {
+
+//        this.calendarProviderPersistenceFactory.get().getEvents(adminEmail);
+
+    }
+
 }
