dc1522183cd3c110a4060619214bd60e8881627b
herv16 <olouherv@gmail.com>
2020-12-07 22:54:50 +0100

    Add getEvents endpoint and test

--- src/main/java/com/getkhaki/api/bff/web/CalendarEventController.java
+++ src/main/java/com/getkhaki/api/bff/web/CalendarEventController.java
@@ -2,6 +2,7 @@ package com.getkhaki.api.bff.web;
 
 import com.getkhaki.api.bff.domain.models.CalendarEventDm;
 import com.getkhaki.api.bff.web.models.CalendarEventDto;
+import com.google.api.services.calendar.model.Events;
 import com.getkhaki.api.bff.domain.services.CalendarEventService;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,15 +20,31 @@ public class CalendarEventController {
         this.modelMapper = modelMapper;
     }
 
-    @GetMapping
-    public String getHello() {
-        return "hello";
-    }
+    // @GetMapping
+    // public String getHello() {
+    //     return "hello";
+    // }
 
     @PostMapping
     public CalendarEventDto createEvent(@RequestBody CalendarEventDto calendarEventDto) {
         CalendarEventDm calendarEventDm = modelMapper.map(calendarEventDto, CalendarEventDm.class);
         CalendarEventDm calendarEventDmReturn = calendarEventService.createEvent(calendarEventDm);
         return modelMapper.map(calendarEventDmReturn, CalendarEventDto.class);
+    }
+
+    @GetMapping
+    public List<CalendarEventDto> getEvents(){
+        List<CalendarEventDto> events = new ArrayList<>();
+        //get events
+        Events allEvents = calendarEventService.events().list("primary")
+                .setTimeMin(now)
+                .setSingleEvents(true)
+                .execute();
+        List<CalendarEventDm> items = allEvents.getItems();
+        //events from calendarEventDm to calendarEventDto
+        for(CalendarEventDm item : items){
+            events.add(modelMapper.map(calendarEventDmReturn, CalendarEventDto.class));
+        }
+        return events;
     }
 }
