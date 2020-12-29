89aa430765b5927dfbbdc541bbd0c2ee6298dd5d
Jacob Boswell <jacob@s56.net>
2020-12-29 15:39:57 -0700

    got organizer stats integration test passing

--- src/main/java/com/getkhaki/api/bff/persistence/repositories/OrganizerStatisticsRepositoryInterface.java
+++ src/main/java/com/getkhaki/api/bff/persistence/repositories/OrganizerStatisticsRepositoryInterface.java
@@ -41,40 +41,6 @@ public interface OrganizerStatisticsRepositoryInterface extends JpaRepository<Ca
                     "           and organizerTotalSecondsCepD.calendarEvent.start between :sDate and :eDate" +
                     "           and organizationTotalSeconds.id = :tenantId" +
                     "   ) as totalSeconds," +
-                    "   (" +
-                    "       (" +
-                    "           select" +
-                    "               sum(" +
-                    "                      timestampdiff(" +
-                    "                          second," +
-                    "                          participantsTotalSecondsCepD.calendarEvent.start," +
-                    "                          participantsTotalSecondsCepD.calendarEvent.end" +
-                    "                      )" +
-                    "               )" +
-                    "           from CalendarEventParticipantDao organizerTotalSeconds2CepD" +
-                    "               left join CalendarEventParticipantDao participantsTotalSecondsCepD " +
-                    "                   on organizerTotalSeconds2CepD.calendarEvent = participantsTotalSecondsCepD.calendarEvent " +
-                    "               inner join participantsTotalSecondsCepD.email.domain.organizations as organizationTotalSeconds" +
-                    "           where organizerTotalSeconds2CepD.email = organizerCalendarEventParticipantDao.email" +
-                    "               and organizerTotalSeconds2CepD.organizer = true" +
-                    "               and organizerTotalSeconds2CepD.calendarEvent.start between :sDate and :eDate" +
-                    "               and organizationTotalSeconds.id = :tenantId" +
-                    "       )" +
-                    "       * " +
-                    "       ((" +
-                    "           select sum(participantsSalaryGroupD.salary)" +
-                    "           from CalendarEventParticipantDao organizerTotalSecondsCostCepD" +
-                    "               left join CalendarEventParticipantDao participantsTotalSecondsCostCepD " +
-                    "                   on organizerTotalSecondsCostCepD.calendarEvent = participantsTotalSecondsCostCepD.calendarEvent " +
-                    "               inner join participantsTotalSecondsCostCepD.email participantsTotalSecondsCostEmailD" +
-                    "               inner join participantsTotalSecondsCostEmailD.people participantsTotalSecondsCostPeople" +
-                    "               inner join participantsTotalSecondsCostPeople.employee participantsTotalSecondsCostEmployeeD" +
-                    "               inner join participantsTotalSecondsCostEmployeeD.salaryGroup participantsSalaryGroupD" +
-                    "           where organizerTotalSecondsCostCepD.email = organizerCalendarEventParticipantDao.email" +
-                    "               and organizerTotalSecondsCostCepD.organizer = true" +
-                    "               and organizerTotalSecondsCostCepD.calendarEvent.start between :sDate and :eDate" +
-                    "       ) / 7200000)" + // seconds in a work year
-                    "   ) as totalCost," +
                     "   concat(organizerEmailDao.user, '@', organizerDomainDao.name) as organizerEmail, " +
                     "   concat(peopleDao.firstName) as organizerFirstName, " +
                     "   concat(peopleDao.lastName) as organizerLastName " +
@@ -102,90 +68,92 @@ public interface OrganizerStatisticsRepositoryInterface extends JpaRepository<Ca
     Page<OrganizerStatisticsView> findExternalOrganizerStatistics(Instant sDate, Instant eDate, UUID tenantId, Pageable pageable);
 
 
-//    @Query(
-//            value = "select " +
-//                    "   (" +
-//                    "       select count(organizerMeetSecondsCepD.id) " +
-//                    "       from CalendarEventParticipantDao organizerMeetSecondsCepD " +
-//                    "       where organizerMeetSecondsCepD.email = organizerCalendarEventParticipantDao.email " +
-//                    "           and organizerMeetSecondsCepD.organizer = true " +
-//                    "           and organizerMeetSecondsCepD.calendarEvent.start between :sDate and :eDate" +
-//                    "   ) as totalMeetings," +
-//                    "   (" +
-//                    "       select" +
-//                    "           sum(" +
-//                    "                   timestampdiff(" +
-//                    "                       second," +
-//                    "                       totalSecondsParticipantsCepD.calendarEvent.start," +
-//                    "                       totalSecondsParticipantsCepD.calendarEvent.end" +
-//                    "                   )" +
-//                    "           )" +
-//                    "       from CalendarEventParticipantDao organizerTotalSecondsCepD" +
-//                    "           left join CalendarEventParticipantDao totalSecondsParticipantsCepD " +
-//                    "               on organizerTotalSecondsCepD.calendarEvent = totalSecondsParticipantsCepD.calendarEvent " +
-//                    "           inner join totalSecondsParticipantsCepD.email.domain.organizations as organizationTotalSeconds" +
-//                    "       where organizerTotalSecondsCepD.email = organizerCalendarEventParticipantDao.email" +
-//                    "           and organizerTotalSecondsCepD.organizer = true" +
-//                    "           and organizerTotalSecondsCepD.calendarEvent.start between :sDate and :eDate" +
-//                    "           and organizationTotalSeconds.id = :tenantId" +
-//                    "   ) as totalSeconds," +
-//                    "   (" +
-//                    "       (" +
-//                    "           select" +
-//                    "               sum(" +
-//                    "                      timestampdiff(" +
-//                    "                          second," +
-//                    "                          participantsTotalSecondsCepD.calendarEvent.start," +
-//                    "                          participantsTotalSecondsCepD.calendarEvent.end" +
-//                    "                      )" +
-//                    "               )" +
-//                    "           from CalendarEventParticipantDao organizerTotalSeconds2CepD" +
-//                    "               left join CalendarEventParticipantDao participantsTotalSecondsCepD " +
-//                    "                   on organizerTotalSeconds2CepD.calendarEvent = participantsTotalSecondsCepD.calendarEvent " +
-//                    "               inner join participantsTotalSecondsCepD.email.domain.organizations as organizationTotalSeconds" +
-//                    "           where organizerTotalSeconds2CepD.email = organizerCalendarEventParticipantDao.email" +
-//                    "               and organizerTotalSeconds2CepD.organizer = true" +
-//                    "               and organizerTotalSeconds2CepD.calendarEvent.start between :sDate and :eDate" +
-//                    "               and organizationTotalSeconds.id = :tenantId" +
-//                    "       )" +
-//                    "       * " +
-//                    "       ((" +
-//                    "           select sum(participantsSalaryGroupD.salary)" +
-//                    "           from CalendarEventParticipantDao organizerTotalSecondsCostCepD" +
-//                    "               left join CalendarEventParticipantDao participantsTotalSecondsCostCepD " +
-//                    "                   on organizerTotalSecondsCostCepD.calendarEvent = participantsTotalSecondsCostCepD.calendarEvent " +
-//                    "               inner join participantsTotalSecondsCostCepD.email participantsTotalSecondsCostEmailD" +
-//                    "               inner join participantsTotalSecondsCostEmailD.people participantsTotalSecondsCostPeople" +
-//                    "               inner join participantsTotalSecondsCostPeople.employee participantsTotalSecondsCostEmployeeD" +
-//                    "               inner join participantsTotalSecondsCostEmployeeD.salaryGroup participantsSalaryGroupD" +
-//                    "           where organizerTotalSecondsCostCepD.email = organizerCalendarEventParticipantDao.email" +
-//                    "               and organizerTotalSecondsCostCepD.organizer = true" +
-//                    "               and organizerTotalSecondsCostCepD.calendarEvent.start between :sDate and :eDate" +
-//                    "       ) / 7200000)" + // seconds in a work year
-//                    "   ) as totalCost," +
-//                    "   concat(organizerEmailDao.user, '@', organizerDomainDao.name) as organizerEmail, " +
-//                    "   concat(peopleDao.firstName) as organizerFirstName, " +
-//                    "   concat(peopleDao.lastName) as organizerLastName " +
-//                    "from CalendarEventParticipantDao organizerCalendarEventParticipantDao " +
-//                    "   inner join organizerCalendarEventParticipantDao.email organizerEmailDao" +
-//                    "   inner join organizerEmailDao.domain organizerDomainDao" +
-//                    "   inner join organizerCalendarEventParticipantDao.calendarEvent organizerCalendarEvent " +
-//                    "   inner join organizerDomainDao.organizations tenant " +
-//                    "   inner join organizerEmailDao.people peopleDao " +
-//                    "where tenant.id = :tenantId" +
-//                    "   and organizerCalendarEventParticipantDao.organizer = true " +
-//                    "   and organizerCalendarEventParticipantDao.calendarEvent.start between :sDate and :eDate " +
-//                    "group by organizerCalendarEventParticipantDao.email",
-//            countQuery = "select count(distinct cepd.email)" +
-//                    " from CalendarEventParticipantDao cepd" +
-//                    "   inner join cepd.calendarEvent ced" +
-//                    "   inner join cepd.email ed" +
-//                    "   inner join ed.people" +
-//                    "   inner join ed.domain dd" +
-//                    "   inner join dd.organizations org " +
-//                    "where org.id = :tenantId" +
-//                    "   and ced.start between :sDate and :eDate" +
-//                    "   and cepd.organizer = true"
-//    )
-//    Page<OrganizerStatisticsView> findAllOrganizerStatistics(Instant sDate, Instant eDate, UUID tenantId, Pageable pageable);
+    @Query(
+            value = "select " +
+                    "   (" +
+                    "       select count(organizerMeetSecondsCepD.id) " +
+                    "       from CalendarEventParticipantDao organizerMeetSecondsCepD " +
+                    "       where organizerMeetSecondsCepD.email = organizerCalendarEventParticipantDao.email " +
+                    "           and organizerMeetSecondsCepD.organizer = true " +
+                    "           and organizerMeetSecondsCepD.calendarEvent.start between :sDate and :eDate" +
+                    "           and exists (" +
+                    "               select count(distinct domain.name)" +
+                    "               from CalendarEventDao innerCalendarEvent" +
+                    "               inner join innerCalendarEvent.participants innerParticipants" +
+                    "               inner join innerParticipants.email.domain domain" +
+                    "               where innerCalendarEvent = organizerMeetSecondsCepD.calendarEvent" +
+                    "               group by innerCalendarEvent" +
+                    "               having count(distinct domain.name) = 1" +
+                    "           )" +
+                    "   ) as totalMeetings," +
+                    "   (" +
+                    "       select" +
+                    "           sum(" +
+                    "                   timestampdiff(" +
+                    "                       second," +
+                    "                       totalSecondsParticipantsCepD.calendarEvent.start," +
+                    "                       totalSecondsParticipantsCepD.calendarEvent.end" +
+                    "                   )" +
+                    "           )" +
+                    "       from CalendarEventParticipantDao organizerTotalSecondsCepD" +
+                    "           left join CalendarEventParticipantDao totalSecondsParticipantsCepD " +
+                    "               on organizerTotalSecondsCepD.calendarEvent = totalSecondsParticipantsCepD.calendarEvent " +
+                    "           inner join totalSecondsParticipantsCepD.email.domain.organizations as organizationTotalSeconds" +
+                    "       where organizerTotalSecondsCepD.email = organizerCalendarEventParticipantDao.email" +
+                    "           and organizerTotalSecondsCepD.organizer = true" +
+                    "           and organizerTotalSecondsCepD.calendarEvent.start between :sDate and :eDate" +
+                    "           and organizationTotalSeconds.id = :tenantId" +
+                    "           and exists (" +
+                    "               select count(distinct domain.name)" +
+                    "               from CalendarEventDao innerCalendarEvent" +
+                    "               inner join innerCalendarEvent.participants innerParticipants" +
+                    "               inner join innerParticipants.email.domain domain" +
+                    "               where innerCalendarEvent = organizerTotalSecondsCepD.calendarEvent" +
+                    "               group by innerCalendarEvent" +
+                    "               having count(distinct domain.name) = 1" +
+                    "           )" +
+                    "   ) as totalSeconds," +
+                    "   concat(organizerEmailDao.user, '@', organizerDomainDao.name) as organizerEmail, " +
+                    "   concat(peopleDao.firstName) as organizerFirstName, " +
+                    "   concat(peopleDao.lastName) as organizerLastName " +
+                    "from CalendarEventParticipantDao organizerCalendarEventParticipantDao " +
+                    "   inner join organizerCalendarEventParticipantDao.email organizerEmailDao" +
+                    "   inner join organizerEmailDao.domain organizerDomainDao" +
+                    "   inner join organizerCalendarEventParticipantDao.calendarEvent organizerCalendarEvent " +
+                    "   inner join organizerDomainDao.organizations tenant " +
+                    "   inner join organizerEmailDao.people peopleDao " +
+                    "where tenant.id = :tenantId" +
+                    "   and organizerCalendarEventParticipantDao.organizer = true " +
+                    "   and organizerCalendarEventParticipantDao.calendarEvent.start between :sDate and :eDate " +
+                    "   and exists (" +
+                    "       select count(distinct domain.name)" +
+                    "       from CalendarEventDao innerCalendarEvent" +
+                    "       inner join innerCalendarEvent.participants innerParticipants" +
+                    "       inner join innerParticipants.email.domain domain" +
+                    "       where innerCalendarEvent = organizerCalendarEvent" +
+                    "       group by innerCalendarEvent" +
+                    "       having count(distinct domain.name) = 1" +
+                    "   )" +
+                    "group by organizerCalendarEventParticipantDao.email",
+            countQuery = "select count(distinct cepd.email)" +
+                    " from CalendarEventParticipantDao cepd" +
+                    "   inner join cepd.calendarEvent ced" +
+                    "   inner join cepd.email ed" +
+                    "   inner join ed.people" +
+                    "   inner join ed.domain dd" +
+                    "   inner join dd.organizations org " +
+                    "where org.id = :tenantId" +
+                    "   and ced.start between :sDate and :eDate" +
+                    "   and cepd.organizer = true" +
+                    "   and exists (" +
+                    "       select count(distinct domain.name)" +
+                    "       from CalendarEventDao innerCalendarEvent" +
+                    "       inner join innerCalendarEvent.participants innerParticipants" +
+                    "       inner join innerParticipants.email.domain domain" +
+                    "       where innerCalendarEvent = ced" +
+                    "       group by innerCalendarEvent" +
+                    "       having count(distinct domain.name) = 1" +
+                    "   )"
+    )
+    Page<OrganizerStatisticsView> findInternalOrganizerStatistics(Instant sDate, Instant eDate, UUID tenantId, Pageable pageable);
 }
--- src/test/java/com/getkhaki/api/bff/persistence/repositories/OrganizerStatisticsRepositoryInterfaceIntegrationTests.java
+++ src/test/java/com/getkhaki/api/bff/persistence/repositories/OrganizerStatisticsRepositoryInterfaceIntegrationTests.java
@@ -20,54 +20,83 @@ public class OrganizerStatisticsRepositoryInterfaceIntegrationTests extends Base
     private OrganizerStatisticsRepositoryInterface underTest;
 
     @Test
-    public void queryTest() {
+    public void testFindExternal() {
         Instant start = Instant.parse("2020-11-01T00:00:00.000Z");
-        Instant end = Instant.parse("2020-11-10T00:00:00.000Z");
-        Pageable pageable = PageRequest.of(0,2);
+        Instant end = Instant.parse("2020-11-30T00:00:00.000Z");
+        Pageable pageable = PageRequest.of(0, 2);
         Page<OrganizerStatisticsView> stats = underTest.findExternalOrganizerStatistics(
                 start,
                 end,
-                UUID.fromString("d713ace2-0d30-43be-b4ba-db973967d6d4"),
+                s56OrgUuid,
                 pageable
         );
 
         var totalEl = stats.getTotalElements();
         var totalpage = stats.getTotalPages();
-//        assertThat(stats.getTotalElements()).isEqualTo(2);
-        stats.forEach(
-                stat -> {
+        assertThat(stats.getTotalElements()).isEqualTo(2);
+        OrganizerStatisticsView bettyStats = stats
+                .stream()
+                .filter(stat -> {
                     var email = stat.getOrganizerEmail();
-                    var bla = stat.getOrganizerEmail();
-                }
-        );
-//        OrganizerStatisticsView bettyStats = stats
-//                .stream()
-//                .filter(stat -> {
-//                    var email = stat.getOrganizerEmail();
-//                    return stat.getOrganizerEmail().equals("betty@s56.net");
-//                })
-//                .findFirst()
-//                .orElseThrow();
-//        var cost = bettyStats.getTotalCost();
-//        var totalSeconds = bettyStats.getTotalSeconds();
-//        var firstName = bettyStats.getOrganizerFirstName();
-//        assertThat(bettyStats.getTotalCost()).isEqualTo(1282.5);
-//        assertThat(bettyStats.getTotalSeconds()).isEqualTo(32400);
-//        assertThat(bettyStats.getTotalMeetings()).isEqualTo(1);
-//        assertThat(bettyStats.getOrganizerFirstName()).isEqualTo("Betty");
-//        assertThat(bettyStats.getOrganizerLastName()).isEqualTo("Smith");
+                    return stat.getOrganizerEmail().equals("betty@s56.net");
+                })
+                .findFirst()
+                .orElseThrow();
+        assertThat(bettyStats.getTotalSeconds()).isEqualTo(32400);
+        assertThat(bettyStats.getTotalMeetings()).isEqualTo(1);
+        assertThat(bettyStats.getOrganizerFirstName()).isEqualTo("Betty");
+        assertThat(bettyStats.getOrganizerLastName()).isEqualTo("Smith");
 
-//
-//        OrganizerStatisticsView bobStats = stats
-//                .stream()
-//                .filter(stat -> stat.getOrganizerEmail().equals("bob@s56.net"))
-//                .findFirst()
-//                .orElseThrow();
-//        assertThat(bobStats.getOrganizerEmail()).isEqualTo("bob@s56.net");
-//        assertThat(bobStats.getTotalMeetings()).isEqualTo(1);
-//        assertThat(bobStats.getTotalSeconds()).isEqualTo(14400);
-//        assertThat(bobStats.getTotalCost()).isEqualTo(380.0);
-//        assertThat(bobStats.getOrganizerFirstName()).isEqualTo("Bob");
-//        assertThat(bobStats.getOrganizerLastName()).isEqualTo("Jones");
+
+        OrganizerStatisticsView bobStats = stats
+                .stream()
+                .filter(stat -> stat.getOrganizerEmail().equals("bob@s56.net"))
+                .findFirst()
+                .orElseThrow();
+        assertThat(bobStats.getOrganizerEmail()).isEqualTo("bob@s56.net");
+        assertThat(bobStats.getTotalMeetings()).isEqualTo(3);
+        assertThat(bobStats.getTotalSeconds()).isEqualTo(25200);
+        assertThat(bobStats.getOrganizerFirstName()).isEqualTo("Bob");
+        assertThat(bobStats.getOrganizerLastName()).isEqualTo("Jones");
     }
+
+    @Test
+    public void testFindInternal() {
+        Instant start = Instant.parse("2020-11-01T00:00:00.000Z");
+        Instant end = Instant.parse("2020-11-30T00:00:00.000Z");
+        Pageable pageable = PageRequest.of(0, 2);
+        Page<OrganizerStatisticsView> stats = underTest.findInternalOrganizerStatistics(
+                start,
+                end,
+                s56OrgUuid,
+                pageable
+        );
+
+        assertThat(stats.getTotalElements()).isEqualTo(2);
+        OrganizerStatisticsView bettyStats = stats
+                .stream()
+                .filter(stat -> {
+                    var email = stat.getOrganizerEmail();
+                    return stat.getOrganizerEmail().equals(email);
+                })
+                .findFirst()
+                .orElseThrow();
+        assertThat(bettyStats.getTotalSeconds()).isEqualTo(32400);
+        assertThat(bettyStats.getTotalMeetings()).isEqualTo(1);
+        assertThat(bettyStats.getOrganizerFirstName()).isEqualTo("Betty");
+        assertThat(bettyStats.getOrganizerLastName()).isEqualTo("Smith");
+
+
+        OrganizerStatisticsView bobStats = stats
+                .stream()
+                .filter(stat -> stat.getOrganizerEmail().equals("bob@s56.net"))
+                .findFirst()
+                .orElseThrow();
+        assertThat(bobStats.getOrganizerEmail()).isEqualTo("bob@s56.net");
+        assertThat(bobStats.getTotalMeetings()).isEqualTo(1);
+        assertThat(bobStats.getTotalSeconds()).isEqualTo(7200);
+        assertThat(bobStats.getOrganizerFirstName()).isEqualTo("Bob");
+        assertThat(bobStats.getOrganizerLastName()).isEqualTo("Jones");
+    }
+
 }
