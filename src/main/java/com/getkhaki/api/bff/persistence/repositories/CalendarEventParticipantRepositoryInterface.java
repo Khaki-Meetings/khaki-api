645b5a8807ec9275e60e0b5a7df9a9052bb8ba07
Jeff Keller <jeff@jeffkeller.com>
2021-03-15 09:01:23 -0400

    Add findDistinctByCalendarEvent back in

--- src/main/java/com/getkhaki/api/bff/persistence/repositories/CalendarEventParticipantRepositoryInterface.java
+++ src/main/java/com/getkhaki/api/bff/persistence/repositories/CalendarEventParticipantRepositoryInterface.java
@@ -7,9 +7,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
+import java.util.Set;
 import java.util.UUID;
 
 @Repository
 public interface CalendarEventParticipantRepositoryInterface extends JpaRepository<CalendarEventParticipantDao, UUID> {
     Optional<CalendarEventParticipantDao> findDistinctByCalendarEventAndEmail(CalendarEventDao calendarEvent, EmailDao email);
+
+    Set<CalendarEventParticipantDao> findDistinctByCalendarEvent(CalendarEventDao calendarEvent);
 }
