85c9a458d51219b8c5e7ab630dde4fa18db4d030
Jeff Keller <jeff@jeffkeller.com>
2021-05-25 13:43:10 -0400

    API call to get Google user list

--- src/main/java/com/getkhaki/api/bff/persistence/repositories/GoogleDirectoryRepository.java
+++ src/main/java/com/getkhaki/api/bff/persistence/repositories/GoogleDirectoryRepository.java
@@ -1,13 +1,17 @@
 package com.getkhaki.api.bff.persistence.repositories;
 
+import com.getkhaki.api.bff.domain.models.OrganizationDm;
 import com.getkhaki.api.bff.domain.services.GoogleClientFactory;
+import com.getkhaki.api.bff.persistence.OrganizationPersistenceService;
 import com.google.api.services.directory.model.User;
+import com.google.api.services.directory.model.Users;
 import lombok.extern.apachecommons.CommonsLog;
 import org.springframework.stereotype.Service;
 
 import javax.inject.Inject;
 import java.io.IOException;
 import java.security.GeneralSecurityException;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -15,10 +19,13 @@ import java.util.List;
 @CommonsLog
 public class GoogleDirectoryRepository {
     private final GoogleClientFactory googleClientFactory;
+    private final OrganizationPersistenceService organizationPersistenceService;
 
     @Inject
-    public GoogleDirectoryRepository(GoogleClientFactory googleClientFactory) {
+    public GoogleDirectoryRepository(GoogleClientFactory googleClientFactory,
+                                     OrganizationPersistenceService organizationPersistenceService) {
         this.googleClientFactory = googleClientFactory;
+        this.organizationPersistenceService = organizationPersistenceService;
     }
 
     public List<User> getUsers(String adminEmail) {
@@ -26,17 +33,35 @@ public class GoogleDirectoryRepository {
         assert emailParts.length == 2 : "Email could not be split";
 
         try {
-            return this.googleClientFactory.getDirectoryClient(adminEmail)
-                    .users()
-                    .list()
-                    .setDomain(emailParts[1])
-                    .setMaxResults(500)
-                    .execute()
-                    .getUsers();
+
+            List<User> usersList = new ArrayList<User>();
+            String pageToken = null;
+
+            do {
+                Users users = this.googleClientFactory.getDirectoryClient(adminEmail)
+                        .users()
+                        .list()
+                        .setDomain(emailParts[1])
+                        .setMaxResults(500)
+                        .setPageToken(pageToken)
+                        .execute();
+
+                pageToken = users.getNextPageToken();
+                usersList.addAll(users.getUsers());
+
+            } while(pageToken != null && pageToken.length() > 0);
+
+            return usersList;
+
         } catch (GeneralSecurityException | IOException e) {
             log.error(e.getMessage());
 
             return Collections.emptyList();
         }
+    }
+
+    public List<User> getUsers() {
+        OrganizationDm organizationDm = organizationPersistenceService.getOrganization();
+        return this.getUsers(organizationDm.getAdminEmail());
     }
 }
