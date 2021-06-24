eb2c220082e69b37149f66067f54a10c7ce85e56
Jeff Keller <jeff@jeffkeller.com>
2021-06-24 08:45:02 -0400

    Added integration tests for employees with statistics

--- src/test/java/com/getkhaki/api/bff/web/EmployeeControllerIntegrationTests.java
+++ src/test/java/com/getkhaki/api/bff/web/EmployeeControllerIntegrationTests.java
@@ -2,8 +2,7 @@ package com.getkhaki.api.bff.web;
 
 import com.getkhaki.api.bff.BaseMvcIntegrationTest;
 import com.getkhaki.api.bff.config.interceptors.models.SessionTenant;
-import com.getkhaki.api.bff.web.models.EmployeeDto;
-import com.getkhaki.api.bff.web.models.UserProfileResponseDto;
+import com.getkhaki.api.bff.web.models.*;
 import lombok.SneakyThrows;
 import lombok.val;
 import org.junit.jupiter.api.Test;
@@ -14,6 +13,8 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.web.context.WebApplicationContext;
+
+import java.time.Instant;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.Matchers.either;
@@ -55,6 +56,50 @@ public class EmployeeControllerIntegrationTests extends BaseMvcIntegrationTest {
                 .andExpect(jsonPath("$.content[?(@.firstName == 'Bob')]").exists())
                 .andExpect(jsonPath("$.content[?(@.firstName == 'Bob')].lastName").value("Jones"))
                 .andExpect(jsonPath("$.content[?(@.firstName == 'Bob')].department").value("HR"));
+    }
+
+    @Test
+    public void getEmployeesWithStatistics() throws Exception {
+        Instant start = Instant.parse("2020-11-01T00:00:00.000Z");
+        Instant end = Instant.parse("2020-11-30T00:00:00.000Z");
+        String url = String.format("/employees/statistics/%s/%s", start, end);
+
+        var x = mvc.perform(MockMvcRequestBuilders.get(url)
+                .header(SessionTenant.HEADER_KEY, "s56_net")
+                .with(jwt().jwt(getJWT("bob@s56.net")).authorities(new SimpleGrantedAuthority("admin"))));
+
+        mvc.perform(MockMvcRequestBuilders.get(url)
+                .header(SessionTenant.HEADER_KEY, "s56_net")
+                .with(jwt().jwt(getJWT("bob@s56.net")).authorities(new SimpleGrantedAuthority("admin"))))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.content.length()").value(3))
+                .andExpect(jsonPath("$.content[?(@.firstName == 'Bob')]").exists())
+                .andExpect(jsonPath("$.content[?(@.firstName == 'Bob')].lastName").value("Jones"))
+                .andExpect(jsonPath("$.content[?(@.firstName == 'Bob')].department").value("HR"))
+                .andExpect(jsonPath("$.content[?(@.firstName == 'Bob')].totalMeetings").value(4))
+                .andExpect(jsonPath("$.content[?(@.firstName == 'Bob')].totalSeconds").value(25200));
+    }
+
+    @Test
+    public void getEmployeesWithStatisticsForDepartment() throws Exception {
+        Instant start = Instant.parse("2020-11-01T00:00:00.000Z");
+        Instant end = Instant.parse("2020-11-30T00:00:00.000Z");
+        String url = String.format("/employees/statistics/%s/%s?department=HR", start, end);
+
+        var x = mvc.perform(MockMvcRequestBuilders.get(url)
+                .header(SessionTenant.HEADER_KEY, "s56_net")
+                .with(jwt().jwt(getJWT("bob@s56.net")).authorities(new SimpleGrantedAuthority("admin"))));
+
+        mvc.perform(MockMvcRequestBuilders.get(url)
+                .header(SessionTenant.HEADER_KEY, "s56_net")
+                .with(jwt().jwt(getJWT("bob@s56.net")).authorities(new SimpleGrantedAuthority("admin"))))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.content.length()").value(1))
+                .andExpect(jsonPath("$.content[?(@.firstName == 'Bob')]").exists())
+                .andExpect(jsonPath("$.content[?(@.firstName == 'Bob')].lastName").value("Jones"))
+                .andExpect(jsonPath("$.content[?(@.firstName == 'Bob')].department").value("HR"))
+                .andExpect(jsonPath("$.content[?(@.firstName == 'Bob')].totalMeetings").value(4))
+                .andExpect(jsonPath("$.content[?(@.firstName == 'Bob')].totalSeconds").value(25200));
     }
 
     @Test
